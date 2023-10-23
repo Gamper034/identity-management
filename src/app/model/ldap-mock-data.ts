c6d6861896aebe4fc4cc2138ff14754b17f7c135
anthony <anthony.diaz@epsi.fr>
2023-10-23 21:08:10 +0200

    xcxc

---
+++ src/app/model/ldap-mock-data.ts
@@ -0,0 +1,18 @@
+import {UserLdap} from "./user-ldap";
+
+export const LDAP_USERS: UserLdap[] [
+  {
+    login: 'test.v1',
+    nom: 'V1',
+    prenom: string;
+    nomComplet: string;
+    motDePasse: string | null;
+    mail: string;
+    role: string;
+    employeNumero: number;
+    employeNiveau: number;
+    dateEmbauche: string;
+    publisherId: number;
+    active: boolean;
+  }
+  ]
